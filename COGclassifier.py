2c546349453742d187121fdd22d96c7073a3aeec
moshi <sy.41211@gmail.com>
2022-03-20 01:24:34 +0900

    First src commit

---
+++ COGclassifier.py
@@ -0,0 +1,436 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import argparse
+import csv
+import gzip
+import os
+import re
+import shutil
+import subprocess as sp
+from collections import defaultdict
+from dataclasses import astuple, dataclass
+from distutils.version import StrictVersion
+from pathlib import Path
+from typing import Dict, List, Union
+
+import requests
+
+
+def main():
+    """COGclassifier main function"""
+    # Get argument values
+    args = get_args()
+    query_fasta_file: Path = args.input_file
+    outdir: Path = args.outdir
+    download_dir: Path = args.download_dir
+    db_type: str = args.db_type
+    thread_num: int = args.thread_num
+    evalue: float = args.evalue
+
+    outdir.mkdir(exist_ok=True)
+    download_dir.mkdir(exist_ok=True)
+
+    print("# Step1: Download COG & CDD FTP data")
+    # Setup COG functional category files
+    cog_fun_ftp_url = "https://ftp.ncbi.nih.gov/pub/COG/COG2020/data/fun-20.tab"
+    cog_def_ftp_url = "https://ftp.ncbi.nih.gov/pub/COG/COG2020/data/cog-20.def.tab"
+
+    cog_fun_file = ftp_download(cog_fun_ftp_url, download_dir)
+    cog_def_file = ftp_download(cog_def_ftp_url, download_dir)
+
+    # Setup NCBI CDD files
+    cddid_tbl_ftp_url = "https://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz"
+    cog_le_ftp_url = "https://ftp.ncbi.nih.gov/pub/mmdb/cdd/little_endian/Cog_LE.tar.gz"
+    kog_le_ftp_url = "https://ftp.ncbi.nih.gov/pub/mmdb/cdd/little_endian/Kog_LE.tar.gz"
+
+    cddid_tbl_gzfile = ftp_download(cddid_tbl_ftp_url, download_dir)
+    cddid_tbl_file = cddid_tbl_gzfile.with_suffix("")
+    unpack_gzfile(cddid_tbl_gzfile, cddid_tbl_file)
+
+    if db_type == "prokaryotes":
+        cog_le_targz_file = ftp_download(cog_le_ftp_url, download_dir)
+        cog_le_dir = download_dir / "Cog_LE"
+        unpack_targz_file(cog_le_targz_file, cog_le_dir)
+        rpsblast_db = cog_le_dir / "Cog"
+    elif db_type == "eukaryotes":
+        kog_le_targz_file = ftp_download(kog_le_ftp_url, download_dir)
+        kog_le_dir = download_dir / "Kog_LE"
+        unpack_targz_file(kog_le_targz_file, kog_le_dir)
+        rpsblast_db = kog_le_dir / "Kog"
+    else:
+        raise ValueError(
+            f"Invalid db_type '{db_type}'. Please set 'prokaryotes' or 'eukaryotes'."
+        )
+
+    # RPS-BLAST against COG database
+    print("\n# Step2: Running RPS-BLAST against COG database.")
+    rpsblast_result_file = outdir / "rpsblast_result.tsv"
+    rpsblast_cmd = (
+        f"rpsblast -query {query_fasta_file} -db {rpsblast_db} -outfmt 6 "
+        f"-out {rpsblast_result_file} -evalue {evalue} -num_threads {thread_num} "
+    )
+    rpsblast_cmd = rpsblast_cmd + "-mt_mode 1" if has_mt_mode_option() else rpsblast_cmd
+    print(f"$ {rpsblast_cmd}")
+    sp.run(rpsblast_cmd, shell=True)
+    print("Finished RPS-BLAST search.")
+
+    # Extract blast top-hit results
+    blast_results = BlastResult.parse(rpsblast_result_file)
+    top_hit_blast_results = BlastResult.extract_top_hit(blast_results)
+    # top_hit_rpsblast_result_file = outdir / "top_hit_rpsblast_result.tsv"
+    # BlastResult.write(top_hit_rpsblast_result_file, top_hit_blast_results)
+
+    # Classify query sequences into COG functional category
+    cddid2cogid = get_cddid2cogid(cddid_tbl_file)
+    cogid2definition = CogDefinition.parse(cog_def_file)
+    letter2func_category = CogFuncCategory.parse(cog_fun_file)
+
+    classifier_contents = ""
+    for br in top_hit_blast_results:
+        queryid, cddid = br.qaccver, br.saccver.replace("CDD:", "")
+        cogid = cddid2cogid[cddid]
+        cog_def = cogid2definition[cogid]
+        letter = cog_def.func_category[0]
+        cog_fun = letter2func_category[letter]
+        classifier_result = (
+            queryid,
+            cogid,
+            cddid,
+            br.evalue,
+            br.pident,
+            cog_def.gene_name,
+            cog_def.cog_name,
+            letter,
+            cog_fun.description,
+        )
+        classifier_contents += "\t".join([str(cr) for cr in classifier_result]) + "\n"
+
+    classifier_result_file = outdir / "classifier_result.tsv"
+    with open(classifier_result_file, "w") as f:
+        header = (
+            "QUERY_ID\tCOG_ID\tCDD_ID\tEVALUE\tIDENTITY\tGENE_NAME\t"
+            + "COG_NAME\tCOG_LETTER\tCOG_DESCRIPTION"
+        )
+        f.write(header + "\n")
+        f.write(classifier_contents)
+
+    # Summarize statistics
+    all_seq_count = count_fasta_seq(query_fasta_file)
+    hit_seq_count = len(top_hit_blast_results)
+    hit_rate = (hit_seq_count / all_seq_count) * 100
+    print(hit_rate)
+
+
+def ftp_download(url: str, outdir: Union[str, Path], overwrite: bool = False) -> Path:
+    """Download file from FTP site
+
+    Args:
+        url (str): FTP site url for download
+        outdir (Union[str, Path]): Output directory
+        overwrite (bool, optional): Overwrite or not
+
+    Returns:
+        Path: Download file path
+    """
+    os.makedirs(outdir, exist_ok=True)
+    download_file = Path(outdir) / Path(url).name
+    print(f"Download '{url}'")
+
+    if download_file.exists() and not overwrite:
+        print(f"=> Already file exists '{download_file}'.")
+        return download_file
+
+    try:
+        res = requests.get(url, stream=True)
+        with open(download_file, "wb") as f:
+            f.write(res.content)
+        print(f"=> Successfully downloaded '{download_file}'.")
+        return download_file
+    except requests.exceptions.ConnectionError as err:
+        print("Failed to download file. Please check network connection.", err)
+        exit(1)
+
+
+def unpack_gzfile(
+    target_file: Union[str, Path], unpacked_file: Union[str, Path]
+) -> None:
+    """Unpack GZIP file
+
+    Args:
+        target_file (Union[str, Path]): Target file to unpack
+        unpacked_file (Union[str, Path]): Unpacked file
+    """
+    with gzip.open(target_file, "rb") as f:
+        content = f.read()
+    with open(unpacked_file, "wb") as f:
+        f.write(content)
+
+
+def unpack_targz_file(target_file: Union[str, Path], outdir: Union[str, Path]) -> None:
+    """Unpack TARGZ file
+
+    Args:
+        target_file (Union[str, Path]): Traget file to unpack
+        outdir (Union[str, Path]): Output directory for unpacked files
+    """
+    shutil.unpack_archive(target_file, outdir)
+
+
+def has_mt_mode_option() -> bool:
+    """Check installed rpsblast has mt_mode option (blast >= v2.12.0)
+
+    Returns:
+        bool: mt_mode exists or not
+
+    Notes:
+        mt_mode defines multi-thread mode ('split-by-database' or 'split-by-query').
+        See https://www.ncbi.nlm.nih.gov/books/NBK571452/ in details.
+    """
+
+    def _get_rpsblast_version() -> str:
+        output = sp.run("rpsblast -version", shell=True, capture_output=True, text=True)
+        return re.match(r"rpsblast: (\d+.\d+.\d+)", output.stdout).groups()[0]
+
+    rpsblast_version = StrictVersion(_get_rpsblast_version())
+    return rpsblast_version >= StrictVersion("2.12.0")
+
+
+def get_cddid2cogid(cddid_tbl_file: Union[str, Path]) -> Dict[str, str]:
+    """Get CDD_ID to COG_ID dict from CDD_ID conversion table file
+
+    Args:
+        cddid_tbl_file (Union[str, Path]): CDD_ID conversion table file
+
+    Returns:
+        Dict[str, str]: CDD_ID to COG_ID conversion dict
+    """
+    cddid2cogid = defaultdict(str)
+    with open(cddid_tbl_file) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            cddid, accid = row[0], row[1]
+            if accid.startswith("COG") or accid.startswith("KOG"):
+                cddid2cogid[cddid] = accid
+    return cddid2cogid
+
+
+def count_fasta_seq(fasta_file: Union[str, Path]) -> int:
+    """Count fasta sequence number
+
+    Args:
+        fasta_file (Union[str, Path]): Fasta format file
+
+    Returns:
+        int: Fasta sequence number
+    """
+    with open(fasta_file) as f:
+        return len(list(filter(lambda l: l.startswith(">"), f.readlines())))
+
+
+@dataclass
+class CogDefinition:
+    """COG Definition DataClass"""
+
+    cog_id: str
+    func_category: str
+    cog_name: str
+    gene_name: str
+    func_pathway: str
+    pubmed_id: str
+    pdb_id: str
+
+    @staticmethod
+    def parse(cog_def_file: Union[str, Path]) -> Dict[str, CogDefinition]:
+        """Parse COG definition file (cog-20.def.tab)
+
+        Args:
+            cog_def_file (Union[str, Path]): COG definition file
+
+        Returns:
+            Dict[str, CogDefinition]: COG_ID to CogDefinition dict
+        """
+        cog_defs = []
+        with open(cog_def_file, encoding="cp1252") as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                cog_defs.append(CogDefinition(*row))
+        return {cd.cog_id: cd for cd in cog_defs}
+
+
+@dataclass
+class CogFuncCategory:
+    """COG Functional Category DataClass"""
+
+    letter: str
+    color: str
+    description: str
+
+    @staticmethod
+    def parse(cog_fun_file: Union[str, Path]) -> Dict[str, CogFuncCategory]:
+        """Parse COG functional category file (fun-20.tab)
+
+        Args:
+            cog_fun_file (Union[str, Path]): COG functional category file
+
+        Returns:
+            Dict[str, CogFuncCategory]: A letter to CogFuncCategory dict
+        """
+        cog_funs = []
+        with open(cog_fun_file) as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                cog_funs.append(CogFuncCategory(*row))
+        return {cf.letter: cf for cf in cog_funs}
+
+
+@dataclass
+class BlastResult:
+    """Blast Result DataClass"""
+
+    qaccver: str  # Query Accession Version
+    saccver: str  # Subject Accession Version
+    pident: float  # Percent Identity
+    length: int  # Alignment Length
+    mismatch: int
+    gapopen: int
+    qstart: int
+    qend: int
+    sstart: int
+    send: int
+    evalue: float
+    bitscore: float
+
+    @staticmethod
+    def parse(blast_tsv_file: Union[str, Path]) -> List[BlastResult]:
+        """Parse tsv format blast result file
+
+        Args:
+            blast_tsv_file (Union[str, Path]): TSV format blast result file
+        Returns:
+            List[BlastResult]: BlastResult list
+        """
+        blast_results: List[BlastResult] = []
+        with open(blast_tsv_file) as f:
+            reader = csv.reader(f, delimiter="\t")
+            for row in reader:
+                # Ignore header line
+                if row[0].startswith("#"):
+                    continue
+                # Cast each row value to appropriate type
+                typed_row = []
+                for idx, val in enumerate(row):
+                    if 0 <= idx <= 1:
+                        typed_row.append(str(val))
+                    elif 3 <= idx <= 9:
+                        typed_row.append(int(val))
+                    else:
+                        typed_row.append(float(val))
+
+                blast_results.append(BlastResult(*typed_row))
+
+        return blast_results
+
+    @staticmethod
+    def extract_top_hit(blast_results: List[BlastResult]) -> List[BlastResult]:
+        """Extract top hit blast results (no duplicated query info)
+
+        Args:
+            blast_results (List[BlastResult]): Blast search results
+
+        Returns:
+            List[BlastResult]: Top hit blast resutls
+        """
+        top_hits = []
+        top_hit_blast_results = []
+        for br in blast_results:
+            if br.qaccver in top_hits:
+                continue
+            top_hits.append(br.qaccver)
+            top_hit_blast_results.append(br)
+        return top_hit_blast_results
+
+    @staticmethod
+    def write(
+        blast_tsv_outfile: Union[str, Path], blast_results: List[BlastResult]
+    ) -> None:
+        """Write BlastResult list with TSV format
+
+        Args:
+            blast_tsv_outfile (Union[str, Path]): Output tsv file
+            blast_results (List[BlastResult]): Blast results to write
+        """
+        output_contents = ""
+        for br in blast_results:
+            output_contents += "\t".join([str(e) for e in astuple(br)]) + "\n"
+        with open(blast_tsv_outfile, "w") as f:
+            f.write(output_contents)
+
+
+def get_args() -> argparse.Namespace:
+    """Get arguments
+
+    Returns:
+        argparse.Namespace: Argument values
+    """
+    parser = argparse.ArgumentParser(
+        description="Classify protein sequences into COG functional category",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input_file",
+        required=True,
+        type=Path,
+        help="Input query protein fasta file",
+        metavar="",
+    )
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        required=True,
+        type=Path,
+        help="Output directory",
+        metavar="",
+    )
+    default_dl_dir = "./cog_download"
+    parser.add_argument(
+        "--download_dir",
+        type=Path,
+        help=f"Download COG & CDD FTP data directory (Default: '{default_dl_dir}')",
+        default=default_dl_dir,
+        metavar="",
+    )
+    db_types = ["prokaryotes", "eukaryotes"]
+    parser.add_argument(
+        "--db_type",
+        type=str,
+        help="COG database type ('prokaryotes'[default] or 'eukaryotes')",
+        default=db_types[0],
+        choices=db_types,
+        metavar="",
+    )
+    cpu_count = os.cpu_count()
+    default_thread_num = cpu_count - 1 if cpu_count is not None else 1
+    parser.add_argument(
+        "-t",
+        "--thread_num",
+        type=int,
+        help=f"RPS-BLAST num_thread parameter (Default: {default_thread_num})",
+        default=default_thread_num,
+        metavar="",
+    )
+    default_evalue = 1e-2
+    parser.add_argument(
+        "-e",
+        "--evalue",
+        type=float,
+        help=f"RPS-BLAST e-value parameter (Default: {default_evalue})",
+        default=default_evalue,
+        metavar="",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
