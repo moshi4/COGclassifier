a54dcc60e7916f9e9ffce4ac00d1ad2fca3b3018
moshi <sy.41211@gmail.com>
2025-04-19 22:03:35 +0900

    Add png plot test case

--- tests/scripts/test_plot_cog_count_barchart.py
+++ tests/scripts/test_plot_cog_count_barchart.py
@@ -3,9 +3,19 @@ import subprocess as sp
 from pathlib import Path
 
 
-def test_cli(cog_count_file: Path, tmp_path: Path):
-    """Test plot_cog_count_barchart CLI"""
+def test_cli_html(cog_count_file: Path, tmp_path: Path):
+    """Test plot_cog_count_barchart CLI (HTML format)"""
     outfile = tmp_path / "cog_count_barchart.html"
+    cmd = f"plot_cog_count_barchart -i {cog_count_file} -o {outfile} --width 540 --height 340 --bar_width 15"  # noqa: E501
+    cmd_args = shlex.split(cmd)
+    result = sp.run(cmd_args)
+    assert result.returncode == 0
+    assert outfile.exists()
+
+
+def test_cli_png(cog_count_file: Path, tmp_path: Path):
+    """Test plot_cog_count_barchart CLI (PNG format)"""
+    outfile = tmp_path / "cog_count_barchart.png"
     cmd = f"plot_cog_count_barchart -i {cog_count_file} -o {outfile} --width 540 --height 340 --bar_width 15"  # noqa: E501
     cmd_args = shlex.split(cmd)
     result = sp.run(cmd_args)
--- tests/scripts/test_plot_cog_count_piechart.py
+++ tests/scripts/test_plot_cog_count_piechart.py
@@ -3,9 +3,19 @@ import subprocess as sp
 from pathlib import Path
 
 
-def test_cli(cog_count_file: Path, tmp_path: Path):
-    """Test plot_cog_count_piechart CLI"""
+def test_cli_html(cog_count_file: Path, tmp_path: Path):
+    """Test plot_cog_count_piechart CLI (HTML format)"""
     outfile = tmp_path / "cog_count_barchart.html"
+    cmd = f"plot_cog_count_piechart -i {cog_count_file} -o {outfile} --width 380 --height 380 --show_letter --sort"  # noqa: E501
+    cmd_args = shlex.split(cmd)
+    result = sp.run(cmd_args)
+    assert result.returncode == 0
+    assert outfile.exists()
+
+
+def test_cli_png(cog_count_file: Path, tmp_path: Path):
+    """Test plot_cog_count_piechart CLI (PNG format)"""
+    outfile = tmp_path / "cog_count_barchart.png"
     cmd = f"plot_cog_count_piechart -i {cog_count_file} -o {outfile} --width 380 --height 380 --show_letter --sort"  # noqa: E501
     cmd_args = shlex.split(cmd)
     result = sp.run(cmd_args)
